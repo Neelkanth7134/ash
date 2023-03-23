b84606012b242835227d8e05b67441a46844abf9
Neelkanth7134 <118507408+Neelkanth7134@users.noreply.github.com>
2023-03-23 15:21:12 +0530

    Update demo.java
    change

--- practice/src/automation/demo.java
+++ practice/src/automation/demo.java
@@ -5,7 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("good morning");
-		
+		System.out.println("good Afternoon");
 		System .out.println("good night");
 	}
 
