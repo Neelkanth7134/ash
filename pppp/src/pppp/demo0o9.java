3540ba84881f154ed0c78bb84be26c1f50a2cdba
Neelkanth7134 <118507408+Neelkanth7134@users.noreply.github.com>
2023-03-23 15:36:29 +0530

    Update demo0o9.java
    change

--- pppp/src/pppp/demo0o9.java
+++ pppp/src/pppp/demo0o9.java
@@ -5,6 +5,8 @@ public class demo0o9 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Automation");
+		
+		System.out.println("Manual");
 	}
 
 }
